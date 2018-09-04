8d294ba20fe277e2513cf0eae61576889a60acb0
Callum Todd <37618393+CxllumTxdd@users.noreply.github.com>
2018-09-04 11:31:09 +0100

    Script was re-written to integrate BeautifulSoup
    BeautifulSoup is used to further automate the process, as web-scraping is used to obtain data about each release such as Label, Year and Catalog no.

--- rename3.py
+++ rename3.py
@@ -1,6 +1,10 @@
 import os
 import sys
+import time
 import webbrowser
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
 
 # Setup / Welcome function definition
 def Setup():
@@ -15,28 +19,39 @@ def Setup():
 # Process function definition - for each folder in root directory
 def ProcessFolder(current):
     print(DirArray[current])
-    Skip = input("Skip? y/enter: ")
-    if Skip == "y":
+    Skip = input("Skip release? Yes: Anykey + Enter | No: Enter")
+    if Skip:
         os.system('cls' if os.name == 'nt' else 'clear')
         print("~~~~ Music Folder File Renamer ~~~~")
         return
     else:
-        Artist = input("Please enter the name of this Artist.\n")
-        Release = input("Please enter the name of this Release.\n")
-        TempName = Artist + Release
-        print("Please use the search link below to Catalog ID of Vinyl Release.")
-        UrlTemp = "http://www.discogs.com/search/?q=" + Artist.replace(" ", "+") + "+" + Release.replace(" ", "+")
-        print(UrlTemp)
-        webbrowser.open(UrlTemp, new=2, autoraise=True)
-        CATID = input("Please enter Catalog ID for Vinyl Release.\n")
-        Date = input("Enter the year of release.\n")
-        NewName = "[" + CATID + "] " + Artist + " - " + Release + " (" + Date + ")"
+        Artist = input("Please enter the name of this Artist.\n")   #Initial user input of artist and release name
+        Release = input("Please enter the name of this Release.\n") #from displayed directory name.
+        #Creates a discogs search URL for the release, opens the webpage and parses its contents using BeautifulSoup
+        SearchUrl = "http://www.discogs.com/search/?q=" + Artist.replace(" ", "+") + "+" + Release.replace(" ", "+")
+        SearchPage = urlopen(SearchUrl)
+        soup = BeautifulSoup(SearchPage, 'html.parser')
+        result = soup.find("a", {"class": "search_result_title"}).attrs["href"] #Obtains href attribute of the search result
+        #Creates the actual release URL, opens the webpage and parses its contents using BeautifulSoup
+        ResultUrl = "http://www.discogs.com" + result
+        ResultPage = urlopen(ResultUrl)
+        soup2 = BeautifulSoup(ResultPage, "html.parser")
+        #Use of <td> HTML tags on the webpage to access information about the release
+        meta_label = soup2.find("td", {"class": "label has_header"})
+        meta_catno = soup2.find("td", {"class": "catno has_header"})
+        meta_year = soup2.find("td", {"class": "year has_header"})
+        label = meta_label.get_text()
+        catno = meta_catno.get_text()
+        year = meta_year.get_text()
+        #Creates the new directory name
+        NewName = "[" + catno + "] " + Artist + " - " + Release + " (" + year + ")"
+        print(NewName)
+        time.sleep(2)
         os.rename(DirArray[current], NewName)
         os.system('cls' if os.name == 'nt' else 'clear')
         print("~~~~ Music Folder File Renamer ~~~~")
 
-# First code to be executed
-
+#code to be executed
 DirArray = os.listdir(os.getcwd())
 DirArray.remove(sys.argv[0])
 Setup()
